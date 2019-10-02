359b21e4cbdb5ddc131d12ee5dd4717f96f7b70d
Gabriel Alcántara Mueses <gabrielalcantara091@gmail.com>
2019-10-02 13:28:29 -0400

    Added About us page

--- SearchPeople/SearchPeople/SearchPeople/ViewModels/AboutUsPageViewModel.cs
+++ SearchPeople/SearchPeople/SearchPeople/ViewModels/AboutUsPageViewModel.cs
@@ -4,6 +4,9 @@ namespace SearchPeople.ViewModels
 {
     public class AboutUsPageViewModel : IInitialize
     {
+        public static string AboutUsText { get; set; } = "\nApp hecha por Emy Saul y Gabriel.\nSearch" +
+            " People es una app que permite buscar la cara de una o varias personas dentro de " +
+            "una carpeta que contenga fotografías";
         public void Initialize(INavigationParameters parameters)
         {
         }
