ba670c2c89a896baef3bf309cb38717c1f350279
emysaulintec <emysaulintec@gmail.com>
2019-10-04 11:21:46 -0400

    added service

---
+++ SearchPeople/SearchPeople/SearchPeople/Services/IRecognitionAppService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchPeople.Services
+{
+    public interface IRecognitionAppService
+    {
+        Task CreateGroupAsync();
+        Task<List<Stream>> CreatePerson(IEnumerable<Stream> trainingPathPerson, string namePerson);
+        Task SearchPersonInPictures(IEnumerable<FileStream> pathSearchPeople, Action<string, IEnumerable<Person>> processImageAction = null, bool personTogueter = false);
+
+        Task DeleteGroup();
+    }
+}
