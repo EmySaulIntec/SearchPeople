800340e31aec625e9cce5599b51affa4f66de3c8
Gabriel Alcántara Mueses <gabrielalcantara091@gmail.com>
2019-10-11 16:59:59 -0400

    Home master-detail finished

---
+++ SearchPeople/SearchPeople/SearchPeople/Models/MenuItemText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchPeople.Models
+{
+    public class MenuItemText
+    {
+        public string Tittle { get; set; }
+        public MenuItemText(string _tittle)
+        {
+            Tittle = _tittle;
+        }
+    }
+}
